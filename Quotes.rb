9a437866555b26b90e68df78920ae677d09c2761
afuerstenau <alex@fuerstenau-clan.de>
2015-01-21 13:39:46 +0100

    minor changes
    renamed quotes to quotes.txt, removed trailing new line char

--- Quotes.rb
+++ Quotes.rb
@@ -3,16 +3,17 @@
 class Quote
   def initialize(file)
     @quotes = Array.new
-    filename = file.to_s+"quotes"
+    filename = file.to_s+"quotes.txt"
     text=File.open(filename, :encoding => "UTF-8").each(sep="\r") do |quote|
       @quotes << quote
     end
   end
 
   def next_quote
-    @quotes[Random.new.rand(0 .. @quotes.length-1)]
+    "Quote of the day: " + @quotes[Random.new.rand(0 .. @quotes.length-1)]
   end
 end
+
 ARGV.each do|file_parameter|
-  puts Quote.new(file_parameter).next_quote
+  print Quote.new(file_parameter).next_quote.chomp
 end
