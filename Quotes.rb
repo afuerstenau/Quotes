de10879431472ae8923fa297d79f2e86b99d4a65
Alex Fürstenau <alex@familie-fuerstenau.de>
2018-09-11 14:49:29 +0200

    small fix

--- Quotes.rb
+++ Quotes.rb
@@ -1,19 +1,18 @@
-# encoding: utf-8
-
 class Quote
   def initialize(file)
-    @quotes = Array.new
-    filename = file.to_s+"quotes.txt"
-    text=File.open(filename, :encoding => "UTF-8").each(sep="\r") do |quote|
+    @quotes = []
+    filename = file.to_s + 'quotes.txt'
+    File.open(filename, encoding: 'UTF-8').each("\r") do |quote|
+      quote=quote.delete("\n")
       @quotes << quote
     end
   end
 
   def next_quote
-    "Quote of the day: " + @quotes[Random.new.rand(0 .. @quotes.length-1)]
+    'Quote of the day: ' + @quotes[Random.new.rand(0..@quotes.length - 1)]
   end
 end
 
-ARGV.each do|file_parameter|
+ARGV.each do |file_parameter|
   print Quote.new(file_parameter).next_quote.chomp
 end
